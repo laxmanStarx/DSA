43dbe40778dc17f5c930c62e6d950d1da517311a
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-03-16 23:06:43 +0530

    first commitss

---
+++ equvilibrium.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int findequvilibrium(vector<int>& arr, int n){
+    n = arr.size();
+    for(int i = 0;i<n;i++){
+
+        //left sum
+
+        int leftSum = 0;
+
+        for(int j =0;j<i;j++){
+            leftSum += arr[j];
+            
+        }
+
+        //right sum
+
+        int rightSum = 0;
+
+        for(int j = i+1;j<arr.size(); j++ ){
+            rightSum +=arr[j];
+
+
+
+        }
+
+        
+            if(leftSum == rightSum){
+                return i;
+            }
+
+            
+
+
+
+
+
+
+    }
+
+    return -1;
+}
+
+
+int main(){
+
+    vector<int> arr = {-7,1,5,2,-4,3,0};
+
+    cout<< findequvilibrium(arr,7);
+
+
+    return 0;
+}

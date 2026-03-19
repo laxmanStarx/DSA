e623c9ef3ee91711ed8b3b5f78bb9b3690043a50
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-03-19 23:54:40 +0530

    first commitssopssss

---
+++ smallest.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+void smallest(int arr[], int n){
+
+    int min_value = arr[0];
+
+    for(int i = 1; i<n;i++){
+        if(min_value > arr[i]){
+            min_value = arr[i];
+        }
+
+    }
+
+    cout << min_value << endl;
+}
+
+
+
+
+
+
+
+int main()
+
+{
+
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i = 0 ;i<n;i++){
+        cin>> arr[i];
+    }
+
+    smallest(arr, n);
+
+
+
+
+    return 0;
+
+}

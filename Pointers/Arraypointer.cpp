39aee1191af66b321c3b0ebc92d08156ea7a63fc
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-03-22 23:12:09 +0530

    second ashoka

---
+++ Pointers/Arraypointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int arr[] = {1,2,3,4,5};
+    cout<<arr<<" "<<endl;
+    cout<<*arr<< " "<<endl;
+    cout<<*(arr + 1)<<" ";
+
+
+
+    return 0;
+}

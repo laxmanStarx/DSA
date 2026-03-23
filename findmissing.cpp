e312d583fe7a137585d2457e5a50f2ff0bb2fa6b
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-03-23 23:50:55 +0530

    initialise commitsss

--- findmissing.cpp
+++ findmissing.cpp
@@ -1,19 +1,54 @@
-#include<iostream>
-#include<vector>
+// #include <iostream>
+// #include<vector>
+
+// using namespace std;
+
+
+// int main()
+// {
+
+//     vector<int> arr = {5,7,8};
+//     int n = arr.size();
+//     int expected = n*(n+1)/2;
+//     int actual = 0;
+//     for(int i = 0;i<n;i++){
+//         actual +=arr[i];
+//     }
+//     int result =  expected - actual;
+//     cout<<"Missing number would be: "<<result<<endl;
+
+
+
+//     return 0;
+// }
+
+
+
+
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
-    vector<int> arr = {3, 0, 1};
+int findMissing(vector<int>& arr) {
     int n = arr.size();
 
-    int expected = n * (n + 1) / 2;
-    int actual = 0;
+    int xor1 = 0, xor2 = 0;
 
-    for(int i = 0; i < n; i++) {
-        actual += arr[i];
+    for(int i = 0; i <= n; i++) {
+        xor1 ^= i;
     }
 
-    cout << "Missing number: " << expected - actual;
+    for(int x : arr) {
+        xor2 ^= x;
+    }
+
+    return xor1 ^ xor2;
+}
+
+int main() {
+    vector<int> arr = {5, 6, 8};
+
+    cout << "Missing number: " << findMissing(arr) << endl;
 
     return 0;
 }

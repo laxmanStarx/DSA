64436e379ba49fd7f33cdd641b9d01364e34de75
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-03-26 00:06:08 +0530

    initialise commitsssdssh

--- Arrays/test3.cpp
+++ Arrays/test3.cpp
@@ -1,8 +1,135 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+
+// class Test {
+// public:
+
+//      Test(){
+
+//     int x;
+// }
+
+
+// };
+
+// int main() {
+//     Test t;
+//     cout << t.x;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// int main() {
+// // int i = 0;
+// // while(i++ < 5) {
+   
+// // }
+// // cout<<i;
+// // return 0;
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class A {
+// public:
+//     int x;
+// };
+
+// int main() {
+//     A *ptr = new A;   // object in heap
+//     ptr->x = 20;
+
+//     cout << ptr->x;   // 20
+
+//     delete ptr;
+// }
+
+
+
+
+
+
+
+
+
+
+//dynamic object creation
+
+
+
+// class A {
+// public:
+//     int x;
+// };
+
+// int main() {
+//     A *ptr = new A;   // object in heap
+//     ptr->x = 20;
+
+//     cout << ptr->x;   // 20
+
+//     delete ptr;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class A {
+public:
+    static int count;
+
+    A() {
+        count++;
+    }
+};
+
+int A::count = 0;
+
 int main() {
-    char str[] = "Hello";
-    cout << str << endl;
-    return 0;
+    A a1, a2, a3;
+
+    cout << A::count;
 }

59bff8d3d952acf20662dd883a5af8a1acb5ce32
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-03-28 00:42:06 +0530

    initialise commitsssdsshghlj

---
+++ Strings/concatenation.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    string a = "Hello";
+    string b = "World";
+
+    string c = a + " " + b;
+
+
+    cout<<c<<endl;
+
+
+    return 0;
+
+}
--- Strings/stringInput.cpp
+++ Strings/stringInput.cpp
@@ -1,9 +1,28 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     char str[] = "Hello";
+//     cout<<str;
+// }
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
 
-    char str[] = "Hello";
-    cout<<str;
+    string s;
+    cin>> s;
+    // getline(cin, s);
+    cout<<s;
+ cout<<s.length();
+ cout<<s.size()<<endl;
+
+ cout<< s[0];
+
+
+    return 0;
 }

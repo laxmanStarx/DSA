a7ad5107cab177000bb83e4690be86ba2f230cec
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-04-14 00:46:42 +0530

    second commitsdhg

---
+++ Pointers/hello.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+//     typedef int* ptr;
+
+// int a = 10, b = 20;
+// ptr p1 = &a, p2 = &b;
+
+// *p1 = *p2;
+// cout << a << " " << b;
+
+int i = 0;
+
+for(i = 0; i < 5; i++);
+
+cout << i;
+
+
+    return 0;
+}
+
